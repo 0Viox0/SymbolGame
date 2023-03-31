efd121a66583c53aa6a718cf517ba80d83c36b22
Viox <m.d.mihailik@gmail.com>
2023-03-31 11:12:03 +0300

    added some drawing and some animations of the player

---
+++ TheMostComplexSymbolGame/Player.cpp
@@ -0,0 +1,30 @@
+#include "Player.h"
+
+//trying create stickman art
+/*
+ "Tonja Hickey" 
+
+	 _\|/^
+      (_oo
+       |     idle looking right
+      /|\
+       |
+
+     ^\|/_
+     oo_)
+       |     idle looking left
+      /|\
+       |
+ 
+
+             _\|/^
+              (_oo  sitting if idle for a long time
+               |
+               |__i
+
+             ^\|/_
+             oo_)  sitting if idle for a long time
+               |
+            i__|
+
+*/
